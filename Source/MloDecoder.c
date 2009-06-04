97059041fd23b91fe48abf1624117385f9c09dde
bok <bok@b5580c6b-5c20-0410-9966-a5f2b9868221>
2009-06-04 04:08:23 +0000

    linefeed cleanup

--- Source/MloDecoder.c
+++ Source/MloDecoder.c
@@ -391,6 +391,7 @@ MLO_Decoder_DecodeFrame(MLO_Decoder*      decoder,
         result = MLO_SampleBuffer_SetSampleCount(buffer, MLO_DEFS_FRAME_LEN_LONG);
 
         /* Decode frame content */
+
         result = MLO_Decoder_DecodeFrameContent (decoder, &decoder->bitstream, buffer);
      }
 
